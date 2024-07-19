2585f6b48d12c2b6aea595da5ccc4119848ec9f5
Gustavo Barrios <gabarrios2006@gmail.com>
2024-07-19 09:11:19 -0300

    terminando a base para a varredura e ordenação dos elementos do documento em um array, por enquanto ainda sem a recursão para os filhos.

--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-const a:HTMLElement=document.body;
+function varreEReposiciona(elemento:HTMLElement)
+{
+    const filhos:HTMLElement[]=Array.from(elemento.childNodes as NodeListOf<HTMLElement>);
+    filhos.sort((a, b)=>{
+const rectA=a.getBoundingClientRect();
+const rectB=b.getBoundingClientRect();
+if(rectA.top<rectB.top)
+{
+    return -1;
+}
+else
+if(rectA.top>rectB.top)
+{
+    return 1;
+}
+else
+{
+    if(rectA.left<rectB.left)
+    {
+        return -1;
+    }
+    else
+    if(rectA.left>rectB.left)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+    });
+}
+const corpo:HTMLElement=document.body;
+varreEReposiciona(corpo);
