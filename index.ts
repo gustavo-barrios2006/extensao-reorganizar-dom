bcf6be94afbbd8e2437ce6333bfbf9bc80e34ce7
Gustavo Barrios <gabarrios2006@gmail.com>
2024-07-23 09:54:15 -0300

    Terminando funcionalidade dele reordenar os elementos no DOM, por enquanto apenas nos nós raiz da árvore do body, bem como abstraindo a funcionalidade de obter as coordenadas dos elementos em uma função para permitir também tratar outros tipos de nós que não sejam nós de elementos HTML, como TextNodes enpacotando-os em um intervalo com document.createRange.

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-function varreEReposiciona(elemento:HTMLElement)
+function varreEReposiciona(elementoPai:HTMLElement)
 {
-    const filhos:HTMLElement[]=Array.from(elemento.childNodes as NodeListOf<HTMLElement>);
+    const filhos:Node[]=Array.from(elementoPai.childNodes as NodeListOf<Node>);
+    const intervaloOriginal:Range=document.createRange();
+    intervaloOriginal.setStartBefore(filhos[0]);
+    intervaloOriginal.setEndAfter(filhos[filhos.length-1]);
     filhos.sort((a, b)=>{
-const rectA=a.getBoundingClientRect();
-const rectB=b.getBoundingClientRect();
+const rectA=getNodeBoundingRect(a);
+const rectB=getNodeBoundingRect(b);
 if(rectA.top<rectB.top)
 {
     return -1;
@@ -30,6 +33,24 @@ else
     }
 }
     });
+    intervaloOriginal.deleteContents();
+    filhos.reverse().forEach(elemento=>{
+        intervaloOriginal.insertNode(elemento);
+    })
+}
+function getNodeBoundingRect(node:Node):DOMRect
+{
+    if(node instanceof HTMLElement)
+    {
+        return node.getBoundingClientRect();
+    }
+    else
+    if(node instanceof Text)
+    {
+        const range=document.createRange();
+        range.selectNodeContents(node);
+        return range.getBoundingClientRect();
+    }
 }
 const corpo:HTMLElement=document.body;
 varreEReposiciona(corpo);
