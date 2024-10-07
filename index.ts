651009321ecf67022da68daef583da9aad60dbdb
Gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-10-07 09:51:55 -0300

    fix(reordenação de elementos): corrigindo um bug em que ele ficava reordenando os elementos em uma recursão sem ponto de fuga usando um WeakMap. Isso acontecia pois ao reorganizar a ordem dos elementos novas entradas de remoções e inserções no DOM eram geradas, o que acionava a função de callback do MutationObserver novamente. Agora com o WeakMap que guarda uma cópia raza dos elementos usando o método cloneNode para evitar guardar apenas a referência dos mesmos, as entradas do MutationObserver só são realmente processadas pela função varreEReposiciona caso as propriedades top e/ou left do elemento tenham sido modificadas em relação à última cópia raza do elemento salva no WeakMap.

--- index.ts
+++ index.ts
@@ -1,110 +1,112 @@
-function varreEReposiciona(elementoPai:HTMLElement)
-{
-    const filhos:Node[]=Array.from(elementoPai.childNodes as NodeListOf<Node>);
-     const intervaloOriginal:Range=document.createRange();
-    intervaloOriginal.setStartBefore(filhos[0]);
-    intervaloOriginal.setEndAfter(filhos[filhos.length-1]);
-    filhos.sort((a, b)=>{
-const rectA=getNodeBoundingRect(a);
-const rectB=getNodeBoundingRect(b);
-if(rectA.top<rectB.top)
-{
-    return -1;
-}
-else
-if(rectA.top>rectB.top)
-{
-    return 1;
-}
-else
-{
-    if(rectA.left<rectB.left)
-    {
-        return -1;
-    }
-    else
-    if(rectA.left>rectB.left)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
+let mapaElementos = new WeakMap();
+function varreEReposiciona(elementoPai: HTMLElement, eCarregamento:boolean=false) {
+    const filhos: Node[] = Array.from(elementoPai.childNodes as NodeListOf<Node>);
+    const intervaloOriginal: Range = document.createRange();
+    intervaloOriginal.setStartBefore(elementoPai);
+    intervaloOriginal.setEndAfter(elementoPai);
+    filhos.sort((a, b) => {
+        const rectA = getNodeBoundingRect(a);
+        const rectB = getNodeBoundingRect(b);
+        if (rectA.top < rectB.top) {
+            return -1;
+        }
+        else
+            if (rectA.top > rectB.top) {
+                return 1;
+            }
+            else {
+                if (rectA.left < rectB.left) {
+                    return -1;
+                }
+                else
+                    if (rectA.left > rectB.left) {
+                        return 1;
+                    }
+                    else {
+                        return 0;
+                    }
+            }
     });
-    intervaloOriginal.deleteContents();
-    filhos.reverse().forEach(element=>{
-        intervaloOriginal.insertNode(element);
-        if(element instanceof HTMLElement)
+    if (elementoPai instanceof HTMLElement) {
+        var posicaoElemento:DOMRect = mapaElementos.get(elementoPai).getBoundingClientRect();
+        if(posicaoElemento)
         {
-            if((element as HTMLElement).childElementCount>0)
-            {
-                varreEReposiciona(element as HTMLElement);
+        }
+        if (typeof posicaoElemento !== 'undefined') {
+            var posicaoAtualElemento:DOMRect = elementoPai.getBoundingClientRect();
+        }
+    }
+    if (typeof posicaoElemento !== 'undefined' && (posicaoElemento.left !== posicaoAtualElemento.left||posicaoElemento.top!==posicaoAtualElemento.top||eCarregamento))
+    {
+        mapaElementos.set(elementoPai, elementoPai.cloneNode(false));
+        if (typeof posicaoElemento !== 'undefined' && (posicaoElemento.left !== posicaoAtualElemento.left||posicaoElemento.top!==posicaoAtualElemento.top||eCarregamento))
+        {
+        intervaloOriginal.deleteContents();
+    }
+}
+    filhos.reverse().forEach(element => {
+            intervaloOriginal.insertNode(element);
+        if (element instanceof HTMLElement) {
+            if ((element as HTMLElement).childElementCount > 0) {
+                varreEReposiciona(element as HTMLElement, eCarregamento);
             }
         }
     });
 }
-function getNodeBoundingRect(node:Node):DOMRect
-{
-    if(node instanceof HTMLElement)
-    {
+function getNodeBoundingRect(node: Node): DOMRect {
+    if (node instanceof HTMLElement) {
         return node.getBoundingClientRect();
     }
     else
-    if(node instanceof Text)
-    {
-        const range:Range=document.createRange();
-        range.selectNodeContents(node);
-        return range.getBoundingClientRect();
-    }
+        if (node instanceof Text) {
+            const range: Range = document.createRange();
+            range.selectNodeContents(node);
+            return range.getBoundingClientRect();
+        }
 }
-const observer=new MutationObserver((mutacoes:MutationRecord[], observer:MutationObserver)=>{
-    mutacoes.forEach(mutacao=>{
-        if(mutacao.type=="childList")
-        {
+const observer = new MutationObserver((mutacoes: MutationRecord[], observer: MutationObserver) => {
+    mutacoes.forEach(mutacao => {
+        if (mutacao.type == "childList" && !mapaElementos.has(mutacao.target as HTMLElement)) {
             varreEReposiciona(mutacao.target as HTMLElement);
-            Array.from(mutacao.addedNodes).forEach(element=>{
-                resizeObserver.observe(element as Element);
-                let coordenadas:DOMRect=(element as HTMLElement).getBoundingClientRect();
-                let coordenadasPai:DOMRect=element.parentElement.getBoundingClientRect();
-            if(coordenadasPai.top>coordenadas.top||coordenadasPai.left>coordenadas.left)
-            {
-                varreEReposiciona(document.body);
-            }
+            Array.from(mutacao.addedNodes).forEach(element => {
+                if (element instanceof HTMLElement) {
+                    resizeObserver.observe(element as HTMLElement);
+                    let coordenadas: DOMRect = (element as HTMLElement).getBoundingClientRect();
+                    let coordenadasPai: DOMRect = (element.parentElement as HTMLElement).getBoundingClientRect();
+                    if (coordenadasPai.top > coordenadas.top || coordenadasPai.left > coordenadas.left && !mapaElementos.has(element as HTMLElement)) {
+                        varreEReposiciona(document.body);
+                    }
+                }
+            });
+            Array.from(mutacao.removedNodes).forEach(element => {
+                resizeObserver.unobserve(element as HTMLElement);
+                if (mapaElementos.has(element as HTMLElement)) {
+                    //mapaElementos.delete(element as HTMLElement);
+                }
+            });
+        }
+    });
 });
-Array.from(mutacao.removedNodes).forEach(element=>{
-    resizeObserver.unobserve(element as Element);
-});
-}
-});
-});
-observer.observe(document.body, {childList:true, subtree:true});
-var resizeObserver=new ResizeObserver(entries=>{
+observer.observe(document.body, { childList: true, subtree: true });
+var resizeObserver = new ResizeObserver(entries => {
     varreEReposiciona(document.body);
 });
 observaElementosComResizeObserver(document.body);
-function observaElementosComResizeObserver(elementoPai:HTMLElement)
-{
-    if(elementoPai.childElementCount>1)
-    {
+function observaElementosComResizeObserver(elementoPai: HTMLElement) {
+        mapaElementos.set(elementoPai, elementoPai.cloneNode(false));
+    if ((elementoPai.childElementCount > 1) || (elementoPai.childElementCount == 1 && elementoPai.children[0] instanceof HTMLElement)) {
         Array.from(elementoPai.children).forEach(element => {
-            resizeObserver.observe(element, {box: "border-box"});
-            if(element.childElementCount>1)
-            {
+            if (element.childElementCount >= 1) {
                 observaElementosComResizeObserver(element as HTMLElement);
             }
-            else
-            if(element.childElementCount==1)
-            {
-                resizeObserver.observe(element.children[0]);
+            else {
+                resizeObserver.observe(element, { box: "border-box" });
+                mapaElementos.set(element, element.cloneNode(false));
             }
         });
     }
-    else
-    if(elementoPai.childElementCount==1)
-    {
-        resizeObserver.observe(elementoPai.children[0]);
+    else {
+        resizeObserver.observe(elementoPai, { box: "border-box" });
     }
 }
-varreEReposiciona(document.body);
+varreEReposiciona(document.body, true);
